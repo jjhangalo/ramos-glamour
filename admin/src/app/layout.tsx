30c7cadb4113993ce429223b3134cb04d97e3ac3
Bartolomeu Hangalo <bartolomeujose.manilson@gmail.com>
2026-04-05 22:41:29 +0100

    feat: add app icons

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+    <head>
+        <meta name="apple-mobile-web-app-title" content="Ramos Glamour | Administração" />
+    </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
