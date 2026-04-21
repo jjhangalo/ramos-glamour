7f8f2acf64e8d156814316b33f7efcd0a1131d25
Bartolomeu Hangalo <bartolomeujose.manilson@gmail.com>
2026-04-21 17:21:46 +0100

    feat: configure Next.js settings with image remote patterns and custom output tracing for admin and store apps

--- store/next.config.ts
+++ store/next.config.ts
@@ -6,7 +6,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
         hostname: "lh3.googleusercontent.com",
+        pathname: "**",
       },
       {
         protocol: "https",
